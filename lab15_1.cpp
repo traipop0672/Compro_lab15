3e26a5b86f07cee0c2ece08ea464353c3ddd5068
Traipop <traipopwichiansarn@gmail.com>
2026-01-30 15:57:32 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,26 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	
-	
+	int a  = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int *z = x;
+
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z <<"\n" ;
+	cout << &a << " " << (int *)&b << " " << (int *)&c << " " << (int *)&x << " " << (int *)&y << " " << &z << "\n";
+	c ='F';
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z <<"\n" ;
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z <<"\n" ;
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z <<"\n" ;
+	*z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z <<"\n" ;
+
 	return 0;
+
 }
